c5a8de5829e23cb6ebfec673e6ac078df4a207f1
jimmybow <jimmybow@hotmail.com.tw>
2018-03-13 15:58:09 +0800

    'update'

--- README.md
+++ README.md
@@ -17,22 +17,20 @@ import myapriori
 import pandas as pd
 ```
 ```
-data = pd.DataFrame([{'性別':'男', '身高':'低'},{'性別':'女', '身高':'低'},{'性別':'男', '身高':'高'}])
-rules = myapriori.run(data = data, supp = 0.3, conf = 0.8,  lift = 1.3, maxlen = 2)
+df = pd.DataFrame([{'性別':'男', '身高':'低'},{'性別':'女', '身高':'低'},{'性別':'男', '身高':'高'}])
+trans = myapriori.df_to_trans(df)
+rules = myapriori.run(data = trans, supp = 0.3, conf = 0.8,  lift = 1.3, maxlen = 2)
 rules
 ```
 ```
 data = [['性別=男', '身高=低'], ['性別=女', '身高=低'], ['性別=男', '身高=高']]
-rules = myapriori.run(data = data, supp = 0.06, conf = 0.75,  lift = 1.1, maxlen = 2, d_type='list')
+rules = myapriori.run(data = data, supp = 0.06, conf = 0.75,  lift = 1.1, maxlen = 2)
 rules
 ```
 
 # Arguments:
 
-* **data** -- 原始資料
-* **d_type** -- 資料格式，支援以下兩種：
-    * d_type = **"df"**  -- Pandas 資料表 (皆為類別欄位) (**Default**)
-    * d_type = **"list"** -- 交易資料集，可以是 list of list 或 array of list 
+* **data** -- 原始資料，支援兩種格式：pandas.DataFrame 或 list of list (交易資料集)
 * **supp** -- The minimum support of rules (float)
 * **conf** -- The minimum confidence of rules (float)
 * **lift** -- The minimum lift of rules (float)
--- myapriori.py
+++ myapriori.py
@@ -11,20 +11,14 @@ import numpy as np
 import time
 
 # 尋找關聯規則的函數 支援兩種資料格式 
-# d_type = 'df' 一般資料表 (皆為類別欄位)
-# d_type = 'list' 交易資料集，格式為 list of list 或 array of list  
-def run(data, supp, conf, lift, constr = '--', d_type = 'df', maxlen = 10):
-  if d_type == 'df':
-      print(u'\n轉換原始 DataFrame 至 交易資料...') 
-      start = time.clock()
-      trans = pd.get_dummies(data, sparse=True, prefix_sep = '=')
-      col = np.array(trans.columns) 
-      trans = np.array(list(map(lambda x: col[x==1] , trans.as_matrix())))
-      end = time.clock() 
-      print(u'\n轉換完畢，耗時：%0.2f秒' %(end-start)) 
-  elif d_type == 'list': trans = data
-  else : raise NameError("d_type need to be set to 'df' or 'list'")
-
+# type = 'df' 一般資料表 (皆為類別欄位)
+# type = 'list' 交易資料集，格式為 list of list 或 array of list  
+def run(data, supp, conf, lift, maxlen = 10, prefix_sep = '='):
+  d_type = type(data)
+  if d_type == pd.DataFrame:
+      trans = df_to_trans(data, prefix_sep = prefix_sep)
+  else: trans = data
+  
   print(u'\n開始搜索關聯規則...')       
   start = time.clock() 
   results = list(apriori(trans, 
@@ -34,20 +28,28 @@ def run(data, supp, conf, lift, constr = '--', d_type = 'df', maxlen = 10):
                          max_length = maxlen))
   end = time.clock()   
   print(u'\n完成，耗時：%0.2f秒' %(end-start))
-  print(u'\n整理结果...')  
-  rows = []                         
-  for i in results :
-      for j in i.ordered_statistics:
-          s = pd.Series(j)
-          s[4] = i.support
-          rows.append(s)
-
-  results = pd.DataFrame(rows)
-  results.columns = ['lhs', 'rhs', 'confidence', 'lift', 'support']       
-  results = results[ ['lhs', 'rhs', 'support', 'confidence', 'lift' ] ]       
-  results['lhs'] = [list(results['lhs'][i]) for i in range(len(results))]     
-  results['rhs'] = [list(results['rhs'][i])[0] for i in range(len(results))]  
-
+  if len(results) > 0:
+      print(u'\n整理结果...')  
+      rows = []                         
+      for i in results :
+          for j in i.ordered_statistics:
+              s = pd.Series(j)
+              s[4] = i.support
+              rows.append(s)    
+      results = pd.DataFrame(rows)
+      results.columns = ['lhs', 'rhs', 'confidence', 'lift', 'support']       
+      results = results[ ['lhs', 'rhs', 'support', 'confidence', 'lift' ] ]       
+      results['lhs'] = [list(results['lhs'][i]) for i in range(len(results))]     
+      results['rhs'] = [list(results['rhs'][i])[0] for i in range(len(results))]  
   print(u'\n共找出 {} 條關聯規則'.format(len(results)))
   return results
 
+def df_to_trans(data, prefix_sep = '='):
+  print(u'\n轉換原始 DataFrame 至 交易資料...') 
+  start = time.clock()
+  trans = pd.get_dummies(data, sparse=True, prefix_sep = prefix_sep)
+  col = np.array(trans.columns) 
+  trans = np.array(list(map(lambda x: col[x==1] , trans.as_matrix())))
+  end = time.clock() 
+  print(u'\n轉換完畢，耗時：%0.2f秒' %(end-start)) 
+  return trans
